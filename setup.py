37c951692542eb69c8fc0456a0073ab668236378
Zihao Feng <87467979+ZihaoFeng2001@users.noreply.github.com>
2023-01-01 23:09:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ os.system("pip3 install --user tensorflow")
 
 #PuLP
 #os.system("sudo apt install glpk glpk-utils")
-os.system("pip3 install --user pulp")
+os.system("pip3 install --user pulp==2.3")
 
 #networkx
-os.system("pip3 install --user networkx")
+os.system("pip3 install --user networkx==2.5")
 
 #tqdm
-os.system("pip3 install --user tqdm")
+os.system("pip3 install --user tqdm==4.51.0")
+
+#Python 3.8.5, Tensorflow v2.2.0, PuLP 2.3, networkx 2.5, tqdm 4.51.0
